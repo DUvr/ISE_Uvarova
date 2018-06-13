3e5c7e5fcb3681dc7cf2a98148f910ddc962e03f
DUvr <31777703+DUvr@users.noreply.github.com>
2018-06-13 17:54:54 +0400

    Add files via upload

---
+++ EquipmentAdmList.cs
@@ -0,0 +1,90 @@
+﻿using AbstractShopModel;
+using AbstractShopService.BindingModels;
+using AbstractShopService.Interfaces;
+using AbstractShopService.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AbstractShopService.ImplementationsList
+{
+    public class ComponentServiceList : IEquipmentService
+    {
+        private DataListSingleton source;
+
+        public ComponentServiceList()
+        {
+            source = DataListSingleton.GetInstance();
+        }
+
+        public List<EquipmentViewModel> GetList()
+        {
+            List<EquipmentViewModel> result = source.Components
+                .Select(rec => new EquipmentViewModel
+                {
+                    Id = rec.Id,
+                    ComponentName = rec.ComponentName
+                })
+                .ToList();
+            return result;
+        }
+
+        public EquipmentViewModel GetElement(int id)
+        {
+            Equipment element = source.Components.FirstOrDefault(rec => rec.Id == id);
+            if (element != null)
+            {
+                return new EquipmentViewModel
+                {
+                    Id = element.Id,
+                    ComponentName = element.ComponentName
+                };
+            }
+            throw new Exception("Элемент не найден");
+        }
+
+        public void AddElement(EquipmentBindingModel model)
+        {
+            Equipment element = source.Components.FirstOrDefault(rec => rec.ComponentName == model.ComponentName);
+            if (element != null)
+            {
+                throw new Exception("Уже есть компонент с таким названием");
+            }
+            int maxId = source.Components.Count > 0 ? source.Components.Max(rec => rec.Id) : 0;
+            source.Components.Add(new Equipment
+            {
+                Id = maxId + 1,
+                ComponentName = model.ComponentName
+            });
+        }
+
+        public void UpdElement(EquipmentBindingModel model)
+        {
+            Equipment element = source.Components.FirstOrDefault(rec => 
+                                        rec.ComponentName == model.ComponentName && rec.Id != model.Id);
+            if (element != null)
+            {
+                throw new Exception("Уже есть компонент с таким названием");
+            }
+            element = source.Components.FirstOrDefault(rec => rec.Id == model.Id);
+            if (element == null)
+            {
+                throw new Exception("Элемент не найден");
+            }
+            element.ComponentName = model.ComponentName;
+        }
+
+        public void DelElement(int id)
+        {
+            Equipment element = source.Components.FirstOrDefault(rec => rec.Id == id);
+            if (element != null)
+            {
+                source.Components.Remove(element);
+            }
+            else
+            {
+                throw new Exception("Элемент не найден");
+            }
+        }
+    }
+}
---
+++ ZakazchikAdmList.cs
@@ -0,0 +1,90 @@
+﻿using AbstractShopModel;
+using AbstractShopService.BindingModels;
+using AbstractShopService.Interfaces;
+using AbstractShopService.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AbstractShopService.ImplementationsList
+{
+    public class ClientServiceList : IZakazchikService
+    {
+        private DataListSingleton source;
+
+        public ClientServiceList()
+        {
+            source = DataListSingleton.GetInstance();
+        }
+
+        public List<ZakazchikViewModel> GetList()
+        {
+            List<ZakazchikViewModel> result = source.Clients
+                .Select(rec => new ZakazchikViewModel
+                {
+                    Id = rec.Id,
+                    ClientFIO = rec.ZakazchikFIO
+                })
+                .ToList();
+            return result;
+        }
+
+        public ZakazchikViewModel GetElement(int id)
+        {
+            Zakazchik element = source.Clients.FirstOrDefault(rec => rec.Id == id);
+            if(element != null)
+            {
+                return new ZakazchikViewModel
+                {
+                    Id = element.Id,
+                    ClientFIO = element.ZakazchikFIO
+                };
+            }
+            throw new Exception("Элемент не найден");
+        }
+
+        public void AddElement(ZakazchikBindingModel model)
+        {
+            Zakazchik element = source.Clients.FirstOrDefault(rec => rec.ZakazchikFIO == model.ClientFIO);
+            if(element != null)
+            {
+                throw new Exception("Уже есть клиент с таким ФИО");
+            }
+            int maxId = source.Clients.Count > 0 ? source.Clients.Max(rec => rec.Id) : 0;
+            source.Clients.Add(new Zakazchik
+            {
+                Id = maxId + 1,
+                ZakazchikFIO = model.ClientFIO
+            });
+        }
+
+        public void UpdElement(ZakazchikBindingModel model)
+        {
+            Zakazchik element = source.Clients.FirstOrDefault(rec => 
+                                    rec.ZakazchikFIO == model.ClientFIO && rec.Id != model.Id);
+            if (element != null)
+            {
+                throw new Exception("Уже есть клиент с таким ФИО");
+            }
+            element = source.Clients.FirstOrDefault(rec => rec.Id == model.Id);
+            if (element == null)
+            {
+                throw new Exception("Элемент не найден");
+            }
+            element.ZakazchikFIO = model.ClientFIO;
+        }
+
+        public void DelElement(int id)
+        {
+            Zakazchik element = source.Clients.FirstOrDefault(rec => rec.Id == id);
+            if (element != null)
+            {
+                source.Clients.Remove(element);
+            }
+            else
+            {
+                throw new Exception("Элемент не найден");
+            }
+        }
+    }
+}
